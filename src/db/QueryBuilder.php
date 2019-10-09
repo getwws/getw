aa8efbd641d6950fbed589937ef8533f89b0e767
allen <admin@h1cms.com>
2019-10-09 10:51:15 +0800

    bug fix

--- src/db/QueryBuilder.php
+++ src/db/QueryBuilder.php
@@ -68,7 +68,7 @@ class QueryBuilder {
      * @return bool
      */
     public function delete() {
-        $sql = "DELETE FROM {$this->table} " . $this->prepareWhere();
+        $sql = "DELETE FROM {{$this->table}} " . $this->prepareWhere();
         return $this->db->query($sql);
     }
 
@@ -88,7 +88,7 @@ class QueryBuilder {
                 $input_params[$name] = $value;
             }
         }
-        $sql = "UPDATE {$this->table} " . ' SET ' . implode(', ', $placeholders) . $this->prepareWhere();
+        $sql = "UPDATE {{$this->table}} " . ' SET ' . implode(', ', $placeholders) . $this->prepareWhere();
         if (($stm = $this->db->query($sql))) {
             return $stm->rowCount();
         }
@@ -454,7 +454,7 @@ class QueryBuilder {
         } else if (isset($table[0]) && $table[0] == '{') {
             return $table;
         } else if (isset($table[0]) && $table[0] != '{') {
-            return "{{$table}} as $table";
+            return "{{$table}}";
         }
     }
 
